f824fa1670a9331c871b48c8b5ee7b52bee41814
benobiju08-prog <benobiju08@gmail.com>
2025-11-25 16:54:50 +0400

    Solution7-Some counting
    **Create multiple loops to count in different ranges and steps:
    
    0 to 50 (increment 1)
    
    50 to 0 (decrement 1)
    
    30 to 50 (increment 1)
    
    50 to 10 (decrement 2)
    
    100 to 200 (increment 5)
    All loops can be included in a single project.**

--- Exercise 07 - Some Counting/solution.py
+++ Exercise 07 - Some Counting/solution.py
@@ -4,7 +4,7 @@ for i in range(1, 51):
     print(i)
 # displays numbers from 50 to 1 by decrement of -1
 print('loop from 50 to 1 by decrement of -1:')
-for i in range(50, 0, -1):
+for i in range(50, 0, -1) :
     print(i)    
 # displays numbers from 1 to 100 by increment of 1
 print('loop numbers from 30 to 50 by increment of 1:')
